3ddbcc7309c0229df191ae78c44e1273bdd22703
Dan Stronge <dnlstronge@gmail.com>
2023-01-21 10:23:01 +0000

    [x] - add state in App to manage users list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Adduser from './components/users/Adduser';
 import UserList from './components/users/UserList';
 
 function App() {
+  const [usersList, setUsersList] = useState([])
   return (
     <div>
       <Adduser />
